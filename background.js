d48820b2eb6d5c6cf4c66477dae73fdcc26be880
Jacob Karlsson <jacob.karlsson95@gmail.com>
2020-02-11 00:03:24 +0100

    Fix toolbar button

--- background.js
+++ background.js
@@ -3,5 +3,5 @@
 chrome.browserAction.setBadgeBackgroundColor({color: '#eae'});
 
 chrome.browserAction.onClicked.addListener(aTab => {
-  chrome.tabs.create({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true});
+  chrome.tabs.create({'url': '/index.html', 'active': true});
 });
